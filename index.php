2aee5d76a84d697a2c724357913076ea9e4b02d1
Anderson Juhasc <anderson.juhasc@gmail.com>
2015-01-14 14:38:38 -0200

    add index.php login page

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php include 'partials/header.php'; ?>
+
+    <!-- .login -->
+    <form class="login" action="create.php">
+        <!-- .login__head -->
+        <header class="login__head">
+            <h1 class="login__title">Acessar conta</h1>
+        </header>
+        <!-- /.login__head -->
+
+        <!-- .login__body -->
+        <div class="login__body">
+            <!-- .login-form -->
+            <ul class="login-form">
+                <li>
+                    <label for="" class="login-form__label">E-mail:</label>
+                    <input id="" name="" type="text" class="login-form__input input" />
+                </li>
+                <li>
+                    <label for="" class="login-form__label">Senha:</label>
+                    <input id="" name="" type="password" class="login-form__input input" />
+                </li>
+                <li>
+                    <button type="submit">Acessar</button>
+                </li>
+            </ul>
+            <!-- /.login-form -->
+        </div>
+        <!-- /.login__body -->
+    </form>
+    <!-- /.login -->
+
+<?php include 'partials/footer.php'; ?>
